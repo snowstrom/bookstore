a92a9f63d160a101348237793b4a4095a666b9a9
snowstrom <gkl_csu_016312@sina.com>
2016-08-06 08:45:40 +0800

    修改格式

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.disable('etag');
 //app.use('/', routes);
 //app.use('/users', users);
 
